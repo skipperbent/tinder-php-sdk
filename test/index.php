a6bec1c8a9d6ce5efb1089a8ce76f85c580d5fc5
Simon Sessingø <simon.sessingoe@gmail.com>
2017-08-10 02:00:53 +0100

    Updated API-headers (issue: #19) and composer modules.

--- test/index.php
+++ test/index.php
@@ -1,6 +1,6 @@
 <?php
 require '../vendor/autoload.php';
 
-$tinder = new \Pecee\Http\Service\Tinder('654639692', 'CAAGm0PX4ZCpsBABa2ANVTLhXrpDjzbxxu8zGyanB8pLZBiPNSDRxFIQDKhLcC8DxTfoZCtZAI0I9XzIVBs51hywNwikZCNHkrrtDcq9rZCiiSBCsdshv6ISr0HkY3vQeu43D3CY1YZBauFeptZClaUElwUMl2OqPsiXgAKAmS1G04ChZBSQ7F2IC6EnWvkw8jS3H3gQib3ZAlOm6PJIeZBOWA0H');
+$tinder = new \Pecee\Http\Service\Tinder('654639692', 'EAAGm0PX4ZCpsBADXVK3ZBLiwmcV9Jc1gplvzNYpDZBrdaowwzgWQPX7mzGDuedufNbX5m3VCLGkdhiAQ37ye0kNohWIHuqox7zJbnOZB2ByXevNsFMatG8TXcLhAUXZAL94Pj31ut81XftZBwlgV7dUYGyPuus7upZATTYyUry0oAuFkoshIRhkr0vYhEfbkknaTLqgoxZAcpsXVoZAI0cuS3eHh7LaxqEgryGsU9HaUjJPp6BO3VnOUZC3AYXpC1Iw3oZD');
 
 var_dump($tinder->getUser());
